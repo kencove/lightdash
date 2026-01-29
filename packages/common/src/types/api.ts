a1ccc08cc0698378657b7d33069ac6f0cd57c266
Jose Rego <jose@lightdash.com>
2026-01-29 10:48:53 +0000

    feat: add dashboard tile and tab limits

--- packages/common/src/types/api.ts
+++ packages/common/src/types/api.ts
@@ -393,6 +393,10 @@ export type HealthState = {
         csvCellsLimit: number;
         maxPageSize: number;
     };
+    dashboard: {
+        maxTilesPerTab: number;
+        maxTabsPerDashboard: number;
+    };
     pivotTable: {
         maxColumnLimit: number;
     };
